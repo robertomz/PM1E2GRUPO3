ea0a881a64152bff443e874bbf0f557fa0de8743
FERNANDO-PC <fers.cas12599@gmial.com>
2021-11-19 20:38:35 -0600

    Se agregan mapas

---
+++ app/src/main/java/com/example/pm1e2grupo3/MapsActivity.java
@@ -0,0 +1,63 @@
+package com.example.pm1e2grupo3;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.pm1e2grupo3.databinding.ActivityMapsBinding;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Intent intent = getIntent();
+        String nombre = intent.getStringExtra("nombre");
+        Double latitud = Double.parseDouble(intent.getStringExtra("latitud"));
+        Double longitud = Double.parseDouble(intent.getStringExtra("longitud"));
+
+
+        LatLng localizacion = new LatLng(latitud, longitud);
+        mMap.addMarker(new MarkerOptions().position(localizacion).title("Uicación de " + nombre));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(localizacion));
+    }
+}
