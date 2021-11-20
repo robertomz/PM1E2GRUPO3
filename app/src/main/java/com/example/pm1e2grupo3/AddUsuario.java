79d3cd268606794b4b4980c8485c759c14694882
FERNANDO-PC <fers.cas12599@gmial.com>
2021-11-20 00:48:04 -0600

    Se agregan cambios en validaciones y tomar imagenes

--- app/src/main/java/com/example/pm1e2grupo3/AddUsuario.java
+++ app/src/main/java/com/example/pm1e2grupo3/AddUsuario.java
@@ -2,14 +2,17 @@ package com.example.pm1e2grupo3;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -21,7 +24,9 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Looper;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.util.Base64;
 import android.view.View;
@@ -49,7 +54,12 @@ import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -62,6 +72,11 @@ public class AddUsuario extends Activity {
     ImageView img;
     Bitmap bitmap;
     String encodeImage;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int PETICION_ACCESO_CAN = 101;
+    String currentPhotoPath;
+    Uri filepath;
+    Boolean tomarFoto;
 
     LocationManager locationManager;
 
@@ -87,6 +102,9 @@ public class AddUsuario extends Activity {
         txtLongitud = (EditText) findViewById(R.id.txtLongitud);
         img = (ImageView) findViewById(R.id.imgCliente);
 
+        edicionCampos(false);
+        txtLatitud.setEnabled(false);
+        txtLongitud.setEnabled(false);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(AddUsuario.this);
 
         //PERMISO DE UBICACION
@@ -102,7 +120,7 @@ public class AddUsuario extends Activity {
         }
 
         //CARGAR IMAGEN
-        img.setOnClickListener(new View.OnClickListener() {
+        /*img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //OBTENER IMAGEN DE GALERIA
@@ -128,31 +146,140 @@ public class AddUsuario extends Activity {
                         }).check();
                 //FIN OBTENER IMAGEN DE GALERIA
             }
-        });
+        });*/
 
         //GUARDAR
         Button btnGuardar = (Button) findViewById(R.id.btnGuardar);
 
-        btnGuardar.setOnClickListener(view -> saveUsuario());
+        //btnGuardar.setOnClickListener(view -> saveUsuario());
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUsuario();
+            }
+        });
+
+        Button btnAdd = (Button) findViewById(R.id.btnAddImagen);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tomarFoto();
+                edicionCampos(true);
+            }
+        });
     }
+
+    private void tomarFoto() {
+        final CharSequence[] options = { "Tomar Foto", "Seleccionar de galeria","Cancelar" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Agregar una Imagen!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Tomar Foto")) {
+                    permisos();
+                    tomarFoto = true;
+                } else if (options[item].equals("Seleccionar de galeria")) {
+                    //OBTENER IMAGEN DE GALERIA
+                    Dexter.withContext(AddUsuario.this)
+                            .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            .withListener(new PermissionListener() {
+                                @Override
+                                public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                                    Intent intent = new Intent(Intent.ACTION_PICK);
+                                    intent.setType("image/*");
+                                    startActivityForResult(Intent.createChooser(intent, "Buscar Imagen"), 1);
+                                    tomarFoto = false;
+                                }
+
+                                @Override
+                                public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                                }
+
+                                @Override
+                                public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                                    permissionToken.continuePermissionRequest();
+                                }
+                            }).check();
+                    //FIN OBTENER IMAGEN DE GALERIA
+                } else if (options[item].equals("Cancelar")) {
+                    dialog.dismiss();
+                    edicionCampos(false);
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void permisos() {
+        //VALIDAR PERMISO QUE ESTA OTORGADO
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            //otorga el permiso si no lo tengo
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PETICION_ACCESO_CAN);
+        }else{
+            TakePhotoDir();
+        }
+    }
+
+    private void TakePhotoDir() {
+        Intent Intenttakephoto= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (Intenttakephoto.resolveActivity(getPackageManager()) != null){
+            File foto = null;
+            try {
+                foto = createImageFile();
+            }
+            catch (Exception ex){
+                ex.toString();
+            }
+            if (foto!= null){
+                filepath = FileProvider.getUriForFile(getApplicationContext(), "com.example.pm1e2grupo3.fileprovider",foto);
+                Intenttakephoto.putExtra(MediaStore.EXTRA_OUTPUT, filepath);
+                startActivityForResult(Intenttakephoto, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+
+
 
     //INICIO DE OBTENER IMAGEN GALERIA Y OBTENER NOMBRE PARA ALMACENARLA
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            Uri filepath = data.getData();
-
-            try {
-                InputStream inputStream = getContentResolver().openInputStream(filepath);
-                bitmap = BitmapFactory.decodeStream(inputStream);
-                img.setImageBitmap(bitmap);
-                encodeBitmapImage(bitmap);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
         super.onActivityResult(requestCode, resultCode, data);
+        if(tomarFoto){
+            if (requestCode == REQUEST_IMAGE_CAPTURE  && resultCode == RESULT_OK) {
+                Bitmap bitmap;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filepath);
+                    encodeBitmapImage(bitmap);
+                    img.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }else{
+            if (requestCode == 1 && resultCode == RESULT_OK) {
+                filepath = data.getData();
+
+                try {
+                    InputStream inputStream = getContentResolver().openInputStream(filepath);
+                    bitmap = BitmapFactory.decodeStream(inputStream);
+                    img.setImageBitmap(bitmap);
+                    encodeBitmapImage(bitmap);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+
     }
+
 
     private void encodeBitmapImage(Bitmap bitmap) {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
@@ -165,17 +292,21 @@ public class AddUsuario extends Activity {
 
     //GUARDAR USUARIO
     private void saveUsuario() {
-        final String nombre = txtNombre.getText().toString().trim();
-        final String telefono = txtTelefono.getText().toString().trim();
-        final String latitud = txtLatitud.getText().toString().trim();
-        final String longitud = txtLongitud.getText().toString().trim();
+         String nombre = txtNombre.getText().toString().trim();
+         String telefono = txtTelefono.getText().toString().trim();
+         String latitud = txtLatitud.getText().toString().trim();
+         String longitud = txtLongitud.getText().toString().trim();
+
+         boolean pError;
 
         final ProgressDialog progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Cargando...");
 
-        if (nombre.isEmpty() || telefono.isEmpty() || latitud.isEmpty() || longitud.isEmpty() || encodeImage.isEmpty()) {
+        if (nombre.isEmpty() || telefono.isEmpty() || latitud.isEmpty() || longitud.isEmpty() || (encodeImage.isEmpty()||encodeImage == null)) {
             Toast.makeText(AddUsuario.this, "LLene todos los campos!", Toast.LENGTH_SHORT).show();
+            pError = true;
         } else {
+            pError = false;
             progressDialog.show();
             StringRequest request = new StringRequest(Request.Method.POST, url, response -> {
                 if (response.length() > 0) {
@@ -206,8 +337,12 @@ public class AddUsuario extends Activity {
                 }
             };
 
-            RequestQueue requestQueue = Volley.newRequestQueue(AddUsuario.this);
-            requestQueue.add(request);
+            if(!pError){
+                RequestQueue requestQueue = Volley.newRequestQueue(AddUsuario.this);
+                requestQueue.add(request);
+            }
+
+            edicionCampos(false);
         }
     }
 
@@ -215,6 +350,11 @@ public class AddUsuario extends Activity {
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+
+    public void edicionCampos(boolean param){
+        txtNombre.setEnabled(param);
+        txtTelefono.setEnabled(param);
     }
 
     //METODOS PARA OBTENER LA UBICACION
@@ -282,5 +422,17 @@ public class AddUsuario extends Activity {
             .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
         }
     }
-
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir /* directory */);
+        // Save a file: path for use with ACTION_VIEW
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 }
